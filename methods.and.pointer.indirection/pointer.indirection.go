fdac5f3b3aecf9f7b23fd085c97ff1d0617a4834
Dan Collins <thedancollins@hotmail.com>
2018-09-01 15:00:49 -0500

    Added some other cases to the tour exercise

---
+++ methods.and.pointer.indirection/pointer.indirection.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+type Vertex struct {
+	X, Y float64
+}
+
+func (v *Vertex) Scale(f float64) {
+	v.X = v.X * f
+	v.Y = v.Y * f
+}
+
+func ScaleFunc(v *Vertex, f float64) {
+	v.X = v.X * f
+	v.Y = v.Y * f
+}
+
+func main() {
+	v := Vertex{3, 4}
+	v.Scale(2)
+	ScaleFunc(&v, 10)
+
+	p := &Vertex{4, 3}
+	p.Scale(3)
+	ScaleFunc(p, 8)
+
+	fmt.Println(v, p)   //yields {60 80} &{96 72}
+	fmt.Println(&v, *p) //yields &{60 80} {96 72}
+	//fmt.Println(*v, &p)  This will not work on v
+	fmt.Println(v, &p)     //{60 80} 0xc042074018
+	fmt.Println(*(&v), &p) //Yields {60 80} 0xc042004028
+	//fmt.Println(&(*v), &p) //This will not work on v
+	fmt.Println(v, &(*p)) //yields {60 80} &{96 72}
+	fmt.Println(v, *(&p)) //yields {60 80} &{96 72}
+}
