68ee9ec444d7eb1b71cc09a580739426219ca691
Dan Collins <thedancollins@hotmail.com>
2018-09-06 08:53:49 -0500

    latest

---
+++ indirection.values/indirection.values.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type Vertex struct {
+	X, Y float64
+}
+
+func (v Vertex) Abs() float64 {
+	return math.Sqrt(v.X*v.X + v.Y*v.Y)
+}
+
+func AbsFunc(v Vertex) float64 {
+	return math.Sqrt(v.X*v.X + v.Y*v.Y)
+}
+
+func main() {
+	v := Vertex{3, 4}
+	fmt.Println(v.Abs())
+	fmt.Println(AbsFunc(v))
+
+	//
+	p := &Vertex{4, 3}
+	fmt.Println(p.Abs())
+	fmt.Println(AbsFunc(*p))
+
+	v = Vertex{5, 6}
+	p = &v
+	fmt.Println(p.Abs())
+	fmt.Println(AbsFunc(*p))
+}
