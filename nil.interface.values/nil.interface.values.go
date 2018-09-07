c8f5dcc5dec2cc29c9afdddbf7ec672e42d76a3d
Dan Collins <thedancollins@hotmail.com>
2018-09-07 09:05:42 -0500

    Worked as expected - it generated a runtime error and brought up a "panic" method.  So I added lines 9-19 to make it work.  Notice that line 14 only works if the method receiver is (t *T) - it will not work with (t T).

---
+++ nil.interface.values/nil.interface.values.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+type I interface {
+	M()
+}
+
+type T struct {
+	S string
+}
+
+func (t *T) M() {
+	if t == nil {
+		fmt.Println("<nil>")
+		return
+	}
+	fmt.Println(t.S)
+}
+
+func main() {
+	var i I
+	describe(i)
+	i = &T{"hello"}
+	describe(i)
+	i.M()
+}
+
+func describe(i I) {
+	fmt.Printf("(%v, %T)\n", i, i)
+}
