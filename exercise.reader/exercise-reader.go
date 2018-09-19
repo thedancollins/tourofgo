0cd42067d7550d10e5bce4270d72383d58559fd1
Dan Collins <thedancollins@hotmail.com>
2018-09-19 08:25:23 -0500

    instructions not clear, hands caught in ceiling fan.  What does "emits an infinite stream of the ASCII character 'A'." mean?  I started poking around online trying to figure out what

---
+++ exercise.reader/exercise-reader.go
@@ -0,0 +1,23 @@
+package main
+
+import "golang.org/x/tour/reader"
+import "fmt"
+
+type MyReader struct{}
+
+// TODO: Add a Read([]byte) (int, error) method to MyReader.
+func (MyReader) Read(b []byte) (n int, err error) {
+	
+	var s string = ""
+	
+	for i :=0; i < len(b); i++ {
+		s += "A"
+	}
+	fmt.Printf(s)
+	
+	return len(b), nil
+}
+
+func main() {
+	reader.Validate(MyReader{})
+}
