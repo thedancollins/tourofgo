bf4a021bf1aa598887ab5e368fe905cf40347b5d
Dan Collins <thedancollins@hotmail.com>
2018-10-28 10:47:42 -0500

    First I added a -7 to the s declaration to see if the slice creation would still account for all elements if the count of elements was odd - and it did.  The next thing I did was to add line 17, 19 and change 20 to see if two different channels could be used.  And that worked too.

---
+++ channels/channels.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func sum(s []int, c chan int) {
+	sum := 0
+	for _, v := range s {
+		sum += v
+	}
+	c <- sum // send sum to c
+}
+
+func main() {
+	s := []int{7, 2, 8, -9, 4, 0, -7}
+
+	c := make(chan int)
+	d := make(chan int)
+	go sum(s[:len(s)/2], c)
+	go sum(s[len(s)/2:], d)
+	x, y := <-c, <-d // receive from c and d
+
+	fmt.Println(x, y, x+y)
+}
