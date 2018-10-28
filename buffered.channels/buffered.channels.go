4c31c6995eadbb015c9ff2182d3b57e56aeaa2fc
Dan Collins <thedancollins@hotmail.com>
2018-10-28 11:10:54 -0500

    Interesting result here if you remove the "go " from line 20 and run it and then add it back in and run it again.  When I run it with "go say" my output does not show 1 but it shows 2 and 5.

--- buffered.channels/buffered.channels.go
+++ buffered.channels/buffered.channels.go
@@ -1,15 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
+
+func say(s chan int) {
+	//for i := 0; i < 5; i++ {
+	time.Sleep(100 * time.Millisecond)
+	fmt.Println(<-s)
+	//}
+}
 
 func main() {
 	ch := make(chan int, 2)
 	ch <- 1
 	ch <- 2
-	fmt.Println("Here")
-	fmt.Println(<-ch)
+	fmt.Println("Here1")
+	go say(ch)
+	fmt.Println("Here2")
 	ch <- 5
+	fmt.Println("Here3")
+	fmt.Println(<-ch)
 	fmt.Println("There")
 	fmt.Println(<-ch)
-	fmt.Println(<-ch)
+	//fmt.Println(<-ch)
 }
